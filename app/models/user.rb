7702ab07a5232765c795d43e56811d765eaac762
takuro-1011 <taku1011opal@icloud.com>
2021-01-05 15:36:15 +0900

    新規登録のモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
+  validates :encrypted_password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe '新規登録/ユーザー情報' do
+    it 'nicknameが空だと登録できないこと' do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+
+    it 'emailが空だと登録できない' do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it '重複したemailがある場合登録できないこと' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include 'Email has already been taken'
+    end
+
+    it 'emailに@がないと登録できない' do
+      @user.email = 'testcom'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Email is invalid'
+    end
+
+    it 'passwordが空だと登録できない' do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+
+    it 'passwordが6文字以上であれば登録できること' do
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      expect(@user).to be_valid
+    end
+
+    it 'passwordは半角英数混同であれば登録できること' do
+      @user.password = 'aaa111'
+      @user.password_confirmation = 'aaa111'
+      expect(@user).to be_valid
+    end
+
+    it 'passwordとpassword_confirmationが不一致では登録できないこと' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234576'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+  end
+
+  describe '新規登録/本人情報' do
+    it 'first_nameがない場合は登録できないこと' do
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name can't be blank"
+    end
+
+    it 'last_nameがない場合は登録できないこと' do
+      @user.last_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name can't be blank"
+    end
+
+    it 'first_name_kanaがない場合は登録できないこと' do
+      @user.first_name_kana = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name kana can't be blank"
+    end
+
+    it 'last_name_kanaがない場合は登録できないこと' do
+      @user.last_name_kana = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name kana can't be blank"
+    end
+
+    it 'birthdayがない場合は登録できないこと' do
+      @user.birthday = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Birthday can't be blank"
+    end
+  end
+end
